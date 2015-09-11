e042239772abe9526ab6672c16b13dfc7361eb16
martymcflywa <doc.brown.83@gmail.com>
2015-09-11 17:23:11 +0800

    MVC model sending data to view through controller

--- MVC/MVC.Web.Tutorial/Controllers/TestController.cs
+++ MVC/MVC.Web.Tutorial/Controllers/TestController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVC.Web.Tutorial.Models;
 
 namespace MVC.Web.Tutorial.Controllers
 {
@@ -11,12 +12,17 @@ namespace MVC.Web.Tutorial.Controllers
         public ActionResult Index()
         {
             // can pass data dynamically with ViewData
-            ViewData["helloWorld"] = "Hello World!";
+            ViewData["helloWorld"] = "ViewData message";
             // experiment with ViewBag
-            ViewBag.Message = "ViewBag say's hi!";
+            ViewBag.Message = "ViewBag message";
             // note: don't shadow VieData and ViewBag names, ie "Message",
             // will cause both to be updated
-            return View();
+
+            // passing data from model
+            var model = new TestModel();
+            model.ModelMessage = "Model message";
+            // pass model to view
+            return View(model);
         }
     }
 }
---
+++ MVC/MVC.Web.Tutorial/Models/TestModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Web.Tutorial.Models
+{
+    public class TestModel
+    {
+        public string ModelMessage { get; set; }
+    }
+}
