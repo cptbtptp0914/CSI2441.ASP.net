2a3944939a0a391846fe65118bd7eabd5ad06e36
martymcflywa <doc.brown.83@gmail.com>
2015-09-11 16:35:04 +0800

    MVC initial project nonempty

---
+++ MVC/MVC.Web.Tutorial/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC.Web.Tutorial.Startup))]
+namespace MVC.Web.Tutorial
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
