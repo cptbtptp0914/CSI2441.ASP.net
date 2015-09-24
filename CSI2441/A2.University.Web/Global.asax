2de6750df4440bb464a7ed7e29e305d1dbc2735e
martymcflywa <doc.brown.83@gmail.com>
2015-09-24 16:57:54 +0800

    CSI2441: new mvc project

---
+++ CSI2441/A2.University.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="A2.University.Web.MvcApplication" Language="C#" %>
---
+++ CSI2441/A2.University.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(A2.University.Web.Startup))]
+namespace A2.University.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
