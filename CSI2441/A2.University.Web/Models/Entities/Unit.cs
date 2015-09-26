eb9083292f8a749e0e395b9c6b6a36b3ef5df462
martymcflywa <doc.brown.83@gmail.com>
2015-09-26 13:38:26 +0800

    CSI2441: No more errors, removing old entities

--- CSI2441/A2.University.Web/Models/Entities/Unit.cs
+++ CSI2441/A2.University.Web/Models/Entities/Unit.cs
@@ -7,6 +7,8 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
+using System.ComponentModel.DataAnnotations;
+
 namespace A2.University.Web.Models.Entities
 {
     using System;
@@ -19,13 +21,30 @@ namespace A2.University.Web.Models.Entities
         {
             this.UnitEnrolments = new HashSet<UnitEnrolment>();
         }
-    
+
+        [Key]
+        [Display(Name = "Unit ID")]
+        [Required(ErrorMessage = "The Unit ID field is required.")]
+        [RegularExpression("([A-Z]{3}[0-9]{4})", ErrorMessage = "Must be a valid Unit ID.")]
         public string unit_id { get; set; }
+
+        [Display(Name = "Title")]
+        [Required(ErrorMessage = "The Title field is required.")]
+        [RegularExpression("(^[A-Z^ ][a-zA-Z0-9 ]{5,}[^ ]$)", ErrorMessage = "Must be a valid Title.")]
         public string title { get; set; }
+
+        [Display(Name = "Coordinator")]
+        [Required(ErrorMessage = "The Coordinator field is required.")]
         public long coodinator_id { get; set; }
+
+        [Display(Name = "Credit Points")]
+        [Required(ErrorMessage = "The Credit Points field is required.")]
         public int credit_points { get; set; }
+
+        [Display(Name = "Unit Type")]
+        [Required(ErrorMessage = "The Unit Type field is required.")]
         public long unit_type_id { get; set; }
-    
+
         public virtual Staff Staff { get; set; }
         public virtual UnitType UnitType { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
