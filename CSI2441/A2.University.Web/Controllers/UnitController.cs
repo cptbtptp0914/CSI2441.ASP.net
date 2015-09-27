f96023181c4d3c1612d76d63b29b09e230ca8d29
martymcflywa <doc.brown.83@gmail.com>
2015-09-27 18:10:32 +0800

    CSI2441: Staff name in alphabetical order in dropdownlist

--- CSI2441/A2.University.Web/Controllers/UnitController.cs
+++ CSI2441/A2.University.Web/Controllers/UnitController.cs
@@ -40,7 +40,7 @@ namespace A2.University.Web.Controllers
         public ActionResult Create()
         {
             // show full name in dropdownlist
-            ViewBag.coodinator_id = new SelectList(db.Staff, "staff_id", "fullname");
+            ViewBag.coodinator_id = new SelectList(db.Staff.OrderBy(s => s.firstname), "staff_id", "fullname");
             ViewBag.unit_type_id = new SelectList(db.UnitTypes, "unit_type_id", "title");
             return View();
         }
@@ -59,7 +59,7 @@ namespace A2.University.Web.Controllers
                 return RedirectToAction("Index");
             }
 
-            ViewBag.coodinator_id = new SelectList(db.Staff, "staff_id", "fullname", unit.coodinator_id);
+            ViewBag.coodinator_id = new SelectList(db.Staff.OrderBy(s => s.firstname), "staff_id", "fullname", unit.coodinator_id);
             ViewBag.unit_type_id = new SelectList(db.UnitTypes, "unit_type_id", "title", unit.unit_type_id);
             return View(unit);
         }
@@ -76,7 +76,7 @@ namespace A2.University.Web.Controllers
             {
                 return HttpNotFound();
             }
-            ViewBag.coodinator_id = new SelectList(db.Staff, "staff_id", "fullname", unit.coodinator_id);
+            ViewBag.coodinator_id = new SelectList(db.Staff.OrderBy(s => s.firstname), "staff_id", "fullname", unit.coodinator_id);
             // ViewBag.unit_type_id deprecated, see ViewData below
             ViewBag.unit_type_id = new SelectList(db.UnitTypes, "unit_type_id", "title", unit.unit_type_id);
 
@@ -100,7 +100,7 @@ namespace A2.University.Web.Controllers
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            ViewBag.coodinator_id = new SelectList(db.Staff, "staff_id", "fullname", unit.coodinator_id);
+            ViewBag.coodinator_id = new SelectList(db.Staff.OrderBy(s => s.firstname), "staff_id", "fullname", unit.coodinator_id);
             ViewBag.unit_type_id = new SelectList(db.UnitTypes, "unit_type_id", "title", unit.unit_type_id);
             return View(unit);
         }
