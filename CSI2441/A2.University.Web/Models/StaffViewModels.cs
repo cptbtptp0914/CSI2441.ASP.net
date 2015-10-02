cd9153efdec475234246dca11d0cbd28fd264f03
martymcflywa <doc.brown.83@gmail.com>
2015-10-02 16:52:08 +0800

    CSI2441: implemented StaffDetailsViewModel

---
+++ CSI2441/A2.University.Web/Models/StaffViewModels.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace A2.University.Web.Models
+{
+    public class StaffBaseViewModel
+    {
+        [Display(Name = "Staff ID")]
+        public long staff_id { get; set; }
+
+        [Display(Name = "First name")]
+        [Required(ErrorMessage = "The First name field is required.")]
+        [RegularExpression("(^[a-zA-Z]+$)", ErrorMessage = "Must be a name.")]
+        public string firstname { get; set; }
+
+        [Display(Name = "Surname")]
+        [Required(ErrorMessage = "The Surname field is required.")]
+        [RegularExpression("(^[a-zA-Z]+$)", ErrorMessage = "Must be a name.")]
+        public string surname { get; set; }
+
+        [Display(Name = "Email")]
+        public string email { get; set; }
+
+        // return full name for dropdownlist
+        [Display(Name = "Coordinator")]
+        public string fullname
+        {
+            get { return firstname + " " + surname; }
+        }
+    }
+
+    public class StaffDetailsViewModel : StaffBaseViewModel
+    {
+        
+    }
+
+    public class StaffCreateViewModel
+    {
+        
+    }
+}
