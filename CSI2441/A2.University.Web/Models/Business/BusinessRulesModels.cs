edd043f195eb6158dff741dedb73536139a85122
martymcflywa <doc.brown.83@gmail.com>
2015-10-03 17:16:54 +0800

    CSI2441: implemented grade model

--- CSI2441/A2.University.Web/Models/Business/BusinessRulesModels.cs
+++ CSI2441/A2.University.Web/Models/Business/BusinessRulesModels.cs
@@ -1,6 +1,30 @@
 namespace A2.University.Web.Models.Business
 {
-    public class BusinessRulesModels
+    public static class Grade
     {
+        public static string GetGrade(int mark)
+        {
+            if (mark >= 80)
+            {
+                return "HD";
+            }
+            else if (mark >= 70)
+            {
+                return "D";
+            }
+            else if (mark >= 60)
+            {
+                return "CR";
+            }
+            else if (mark >= 50)
+            {
+                return "C";
+            }
+            else
+            {
+                return "N";
+            }
+
+        }
     }
 }
