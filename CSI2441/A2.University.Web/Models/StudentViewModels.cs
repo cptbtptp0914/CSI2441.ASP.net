96489627e25a1c92e32a8457b4b0ca2c4b0914a8
martymcflywa <doc.brown.83@gmail.com>
2015-10-03 12:20:00 +0800

    CSI2441: removed validation data annotations, to be replaced by fluent

--- CSI2441/A2.University.Web/Models/StudentViewModels.cs
+++ CSI2441/A2.University.Web/Models/StudentViewModels.cs
@@ -20,53 +20,40 @@ namespace A2.University.Web.Models
         public long student_id { get; set; }
 
         [Display(Name = "First name")]
-        [Required(ErrorMessage = "The First name field is required.")]
-        [RegularExpression("(^[a-zA-Z]+$)", ErrorMessage = "Must be a name.")]
         public string firstname { get; set; }
 
         [Display(Name = "Surname")]
-        [Required(ErrorMessage = "The Surname field is required.")]
-        [RegularExpression("(^[a-zA-Z]+$)", ErrorMessage = "Must be a name.")]
         public string lastname { get; set; }
 
         [Display(Name = "DOB")]
-        [DataType(DataType.Date, ErrorMessage = "Must be a date.")]
+        [DataType(DataType.Date]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public System.DateTime dob { get; set; }
 
         [Display(Name = "Gender")]
-        [Required(ErrorMessage = "The Gender field is required.")]
-        [RegularExpression("(M|F)", ErrorMessage = "Must be a valid gender.")]
         public string gender { get; set; }
 
         [Display(Name = "Email")]
         public string email { get; set; }
 
         [Display(Name = "Landline")]
-        [Required(ErrorMessage = "The Landline field is required.")]
         public string ph_landline { get; set; }
 
         [Display(Name = "Mobile")]
-        [Required(ErrorMessage = "The Mobile field is required.")]
         public string ph_mobile { get; set; }
 
         [Display(Name = "Address")]
-        [Required(ErrorMessage = "The Address field is required.")]
         public string adrs { get; set; }
 
         [Display(Name = "City")]
-        [Required(ErrorMessage = "The City field is required.")]
         public string adrs_city { get; set; }
 
         [Display(Name = "State")]
-        [Required(ErrorMessage = "The State field is required.")]
         public string adrs_state { get; set; }
 
-        [Display(Name = "Postcode")]
-        [Required(ErrorMessage = "The Postcode field is required.")]
-        [RegularExpression("(^[0-9]{4}$)", ErrorMessage = "Must be a valid postcode.")]
         // made string instead of int, else displays 0 as prefilled default for some reason
         // cast to int when updating db
+        [Display(Name = "Postcode")]
         public string adrs_postcode { get; set; }
     }
 
