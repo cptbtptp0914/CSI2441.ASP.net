e3973e04c2d5f046e1c93d00b75dfa73779f97b3
martymcflywa <doc.brown.83@gmail.com>
2015-10-04 16:27:47 +0800

    CSI2441: finished CourseEnrolment Create view

--- CSI2441/A2.University.Web/Controllers/CourseEnrolmentController.cs
+++ CSI2441/A2.University.Web/Controllers/CourseEnrolmentController.cs
@@ -157,7 +157,8 @@ namespace A2.University.Web.Controllers
             entityModel.course_enrolment_id = viewModel.course_enrolment_id;
             entityModel.student_id = viewModel.student_id;
             entityModel.course_id = viewModel.course_id;
-            //entityModel.course_status = "ENROLLED"; //viewModel.course_status;
+            // TODO: call function that determines course_status here
+            //entityModel.course_status = "ENROLLED" is default
         }
 
         /// <summary>
