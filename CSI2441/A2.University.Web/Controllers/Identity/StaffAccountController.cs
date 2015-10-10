88bfdd10029af370e1f5b3849eae0618719f0a54
martymcflywa <doc.brown.83@gmail.com>
2015-10-10 21:21:47 +0800

    CSI2441: attempting to implement double login home page

--- CSI2441/A2.University.Web/Controllers/Identity/StaffAccountController.cs
+++ CSI2441/A2.University.Web/Controllers/Identity/StaffAccountController.cs
@@ -5,12 +5,52 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using A2.University.Web.Models;
+using Microsoft.AspNet.Identity.Owin;
 
 namespace A2.University.Web.Controllers.Identity
 {
     [Authorize]
     public class StaffAccountController : AccountController
     {
+        //
+        // GET: /Account/Login
+        [AllowAnonymous]
+        public new ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        //
+        // POST: /Account/Login
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public new async Task<ActionResult> Login(LoginViewModel model, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToLocal(returnUrl);
+            }
+
+            // This doesn't count login failures towards account lockout
+            // To enable password failures to trigger account lockout, change to shouldLockout: true
+            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            switch (result)
+            {
+                case SignInStatus.Success:
+                    return RedirectToAction("Index", "StaffPortal");
+                case SignInStatus.LockedOut:
+                    return View("Lockout");
+                case SignInStatus.RequiresVerification:
+                    return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
+                case SignInStatus.Failure:
+                default:
+                    ModelState.AddModelError("", "Invalid login attempt.");
+                    return View(model);
+            }
+        }
+
         //
         // POST: /Account/Register
         /// <summary>
@@ -44,7 +84,7 @@ namespace A2.University.Web.Controllers.Identity
                         // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                         // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                        return RedirectToAction("Index", "Home");
+                        return RedirectToAction("Index", "StaffPortal");
                     }
                 }
                 AddErrors(result);
