eb8b04d17c85f8ceea623319a85f515cf695c73d
martymcflywa <doc.brown.83@gmail.com>
2015-10-10 18:13:00 +0800

    CSI2441: cleanup

--- CSI2441/A2.University.Web/Controllers/Identity/StudentAccountController.cs
+++ CSI2441/A2.University.Web/Controllers/Identity/StudentAccountController.cs
@@ -21,37 +21,34 @@ namespace A2.University.Web.Controllers.Identity
         [ValidateAntiForgeryToken]
         public new async Task<ActionResult> Register(RegisterViewModel model)
         {
-            using (var context = new ApplicationDbContext())
+            if (ModelState.IsValid)
             {
-                if (ModelState.IsValid)
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var result = await UserManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
                 {
-                    var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
-                    var result = await UserManager.CreateAsync(user, model.Password);
+                    // give student role to user
+                    var userRole = await UserManager.AddToRoleAsync(user.Id, "STUDENT");
 
-                    if (result.Succeeded)
+                    if (userRole.Succeeded)
                     {
-                        // give student role to user
-                        var userRole = await UserManager.AddToRoleAsync(user.Id, "STUDENT");
+                        await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
 
-                        if (userRole.Succeeded)
-                        {
-                            await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+                        // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
+                        // Send an email with this link
+                        // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
+                        // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+                        // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                            // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
-                            // Send an email with this link
-                            // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
-                            // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-                            // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
-
-                            return RedirectToAction("Index", "Home");
-                        }
+                        return RedirectToAction("Index", "Home");
                     }
-                    AddErrors(result);
                 }
-
-                // If we got this far, something failed, redisplay form
-                return View(model);
+                AddErrors(result);
             }
+
+            // If we got this far, something failed, redisplay form
+            return View(model);
         }
     }
 }
