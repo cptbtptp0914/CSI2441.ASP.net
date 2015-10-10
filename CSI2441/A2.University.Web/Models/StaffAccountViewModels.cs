aa85c6bdfc59576c093479578ee9439919eb2873
martymcflywa <doc.brown.83@gmail.com>
2015-10-10 18:56:46 +0800

    CSI2441: inherits from AccountViewModels classes

---
+++ CSI2441/A2.University.Web/Models/StaffAccountViewModels.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace A2.University.Web.Models
+{
+    // inheriting template's AccountViewModels classes for Staff
+
+    public class StaffExternalLoginConfirmationViewModel : ExternalLoginConfirmationViewModel
+    {
+
+    }
+
+    public class StaffExternalLoginListViewModel : ExternalLoginListViewModel
+    {
+        
+    }
+
+    public class StaffSendCodeViewModel : SendCodeViewModel
+    {
+        
+    }
+
+    public class StaffVerifyCodeViewModel : VerifyCodeViewModel
+    {
+        
+    }
+
+    public class StaffForgotViewModel : ForgotViewModel
+    {
+        
+    }
+
+    public class StaffLoginViewModel : LoginViewModel
+    {
+        
+    }
+
+    public class StaffRegisterViewModel : RegisterViewModel
+    {
+        
+    }
+
+    public class StaffResetPasswordViewModel : ResetPasswordViewModel
+    {
+        
+    }
+
+    public class StaffForgotPasswordViewModel : ForgotPasswordViewModel
+    {
+        
+    }
+}
---
+++ CSI2441/A2.University.Web/Models/StudentAccountViewModels.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace A2.University.Web.Models
+{
+    // inheriting template's AccountViewModels classes for Student
+
+    public class StudentExternalLoginConfirmationViewModel : ExternalLoginConfirmationViewModel
+    {
+
+    }
+
+    public class StudentExternalLoginListViewModel : ExternalLoginListViewModel
+    {
+
+    }
+
+    public class StudentSendCodeViewModel : SendCodeViewModel
+    {
+
+    }
+
+    public class StudentVerifyCodeViewModel : VerifyCodeViewModel
+    {
+
+    }
+
+    public class StudentForgotViewModel : ForgotViewModel
+    {
+
+    }
+
+    public class StudentLoginViewModel : LoginViewModel
+    {
+
+    }
+
+    public class StudentRegisterViewModel : RegisterViewModel
+    {
+
+    }
+
+    public class StudentResetPasswordViewModel : ResetPasswordViewModel
+    {
+
+    }
+
+    public class StudentForgotPasswordViewModel : ForgotPasswordViewModel
+    {
+
+    }
+}
