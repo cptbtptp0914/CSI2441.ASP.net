8599294714c3a0d7e55e609e04b7520b1eaa2715
martymcflywa <doc.brown.83@gmail.com>
2015-10-10 18:48:18 +0800

    CSI2441: added StaffPortal Index view

--- CSI2441/A2.University.Web/Controllers/StaffPortalController.cs
+++ CSI2441/A2.University.Web/Controllers/StaffPortalController.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace A2.University.Web.Controllers
 {
-    public class StaffPortalController
+    public class StaffPortalController : Controller
     {
+        // GET: StaffPortal
+        public ActionResult Index()
+        {
+            return View();
+        }
     }
 }
