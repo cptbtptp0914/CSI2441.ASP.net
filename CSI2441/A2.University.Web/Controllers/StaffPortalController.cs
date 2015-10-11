f1200481ff80edb0ecabff244f9a618385b0a5cd
martymcflywa <doc.brown.83@gmail.com>
2015-10-11 20:37:29 +0800

    CSI2441: implemented custom Authorize, redirects to Home Index

--- CSI2441/A2.University.Web/Controllers/StaffPortalController.cs
+++ CSI2441/A2.University.Web/Controllers/StaffPortalController.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using A2.University.Web.Models;
 
 namespace A2.University.Web.Controllers
 {
-    [Authorize(Roles = "STAFF")]
+    [UniAuthorize(Roles = "STAFF")]
     public class StaffPortalController : Controller
     {
         // GET: StaffPortal
---
+++ CSI2441/A2.University.Web/Models/UniAuthorize.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace A2.University.Web.Models
+{
+    public class UniAuthorize : AuthorizeAttribute
+    {
+        
+        /// <summary>
+        /// Overriding Authorize to redirect user to Home Index if unauthorized.
+        /// </summary>
+        /// <param name="filterContext"></param>
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            // if authorized
+            if (this.AuthorizeCore(filterContext.HttpContext))
+            {
+                // call super
+                base.OnAuthorization(filterContext);
+            }
+            else
+            {
+                // custom redirect
+                filterContext.Result = new RedirectResult("~/Home/Index");
+            }
+        }
+    }
+}
