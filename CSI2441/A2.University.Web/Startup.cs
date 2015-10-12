3ba9122d4b37b625c3d862b0ad643d73d5d670f5
martymcflywa <doc.brown.83@gmail.com>
2015-10-12 19:20:57 +0800

    CSI2441: removed all references to LocalDB

--- CSI2441/A2.University.Web/Startup.cs
+++ CSI2441/A2.University.Web/Startup.cs
@@ -8,7 +8,7 @@ namespace A2.University.Web
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
+//            ConfigureAuth(app);
         }
     }
 }
